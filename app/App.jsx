5e89bc621b9ac24311da528dcaf46fd51aca26c9
Vincent Toon <vincenttoon22@gmail.com>
2026-05-12 20:46:06 -0500

    initial website setup

---
+++ app/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        fontFamily: "Arial",
+        padding: "40px",
+        textAlign: "center",
+      }}
+    >
+      <h1>Ruscoob</h1>
+      <p>Website coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
